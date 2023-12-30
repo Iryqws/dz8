5c8bbc705360bf485b1556b325dc58bb1e1b47fe
Iryqws <152629753+Iryqws@users.noreply.github.com>
2023-12-30 14:20:22 +0200

    Add files via upload

---
+++ dz 8.py
@@ -0,0 +1,17 @@
+class Klass:
+
+   def __init__(self, data):
+
+       self.data = data
+
+   def __iter__(self):
+
+       for item in self.data:
+
+           yield item
+
+iterable = Klass([1, 2, 3, 4, 5])
+
+for item in iterable:
+
+   print(item)
